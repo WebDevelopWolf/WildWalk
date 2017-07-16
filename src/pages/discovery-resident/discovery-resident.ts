5f9f04a061f04ca44b7f3915173b352c14846b39
Liane Stevenson <webdevelopwolf@gmail.com>
2017-07-17 00:45:24 +0100

    Profile Complete
    Profile looks and functionality complete

--- src/pages/discovery-resident/discovery-resident.ts
+++ src/pages/discovery-resident/discovery-resident.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { WildWalkApi } from "../../app/shared/shared";
 import { Headers, RequestOptions, Http } from "@angular/http";
@@ -29,10 +29,15 @@ export class DiscoveryResident {
   avgHeight: any;
   avgWeight: any;
   sizegraphic: any;
-  story: any; 
+  story: any;
+  didyouknow: any; 
+  adoptGraphic: any;
+  userid: any;
+  likes: any;
 
-  constructor(public navCtrl: NavController, private wwapi: WildWalkApi, public navParams: NavParams, private http: Http) {
+  constructor(public navCtrl: NavController, private wwapi: WildWalkApi, public navParams: NavParams, private http: Http, private alertCtrl: AlertController) {
     this.id = this.navParams.data;
+    this.userid = 1;
   }
 
   ionViewDidLoad() {
@@ -42,7 +47,6 @@ export class DiscoveryResident {
     this.apiurl = this.wwapi.baseUrl;
     this.wwapi.getRepoData('discovery/resident/' + this.id).subscribe(data => {
         this.resident = data;
-        console.log(this.resident);
         this.picture = data.Picture;
         this.section = data.Discoveryid;
         this.name = data.Name;
@@ -58,7 +62,53 @@ export class DiscoveryResident {
         this.avgWeight = data.Avgweight;
         this.sizegraphic = data.Sizegraphic;
         this.story = data.Story;
+        this.likes = data.Likes;
+        this.wwapi.getRepoData('discovery/dyk/' + data.Profileid).subscribe(dyk => {
+          this.didyouknow = dyk;
+        })
       });
+  }
+
+  // Add resident to current users favourites
+  addToFav() {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.apiurl + '/discovery/addToFavourite/' + this.id + '/' + this.userid,'')
+    .toPromise()
+    .then(response => this.favAlert())
+    .catch(this.handleError);
+  }
+
+  // Add resident to current users favourites
+  likeResident() {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.apiurl + '/discovery/likeResident/' + this.id, '')
+    .toPromise()
+    .then(response => this.likeAlert())
+    .catch(this.handleError);
+  }
+
+  // Once added show an alert confirming the add
+  favAlert() {
+    let alert = this.alertCtrl.create({
+      title: this.name + ' thanks you!',
+      subTitle: 'You added them to you favourites! This could be the start of something special...',
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
+  likeAlert() {
+    this.likes++;
+    let alert = this.alertCtrl.create({
+      title: 'Awww, ' + this.name + ' likes you too!',
+      subTitle: this.name + ' has ' + this.likes + ' likes now!',
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
